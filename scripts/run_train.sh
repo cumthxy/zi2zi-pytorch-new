dfb69ac1b11ef12f9c94cac0f06d1db80f339f3d
hahaha <cumthxy@163.com>
2020-09-08 20:47:19 +0800

    1070json

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -2,7 +2,7 @@
 export CUDA_VISIBLE_DEVICES=2
 
 python train.py \
---experiment_dir ZhongHuaSong2Shufa_experiment \
+--experiment_dir experiment \
 --gpu_ids cuda:0 \
 --epoch 50 \
 --batch_size 64 \
