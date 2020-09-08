e4933996a782d3f7c28c79c71a893cfc849f77b8
hahaha <cumthxy@163.com>
2020-09-08 23:30:03 +0800

    1070json

--- model/losses.py
+++ model/losses.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
-
-
+import torchvision
 class CategoryLoss(nn.Module):
     def __init__(self, category_num):
         super(CategoryLoss, self).__init__()
@@ -15,17 +14,53 @@ class CategoryLoss(nn.Module):
         return self.loss(category_logits, target)
 
 
-class BinaryLoss(nn.Module):
-    def __init__(self, real):
-        super(BinaryLoss, self).__init__()
-        self.bce = nn.BCEWithLogitsLoss()
-        self.real = real
+# class BinaryLoss(nn.Module):
+#     def __init__(self, real):
+#         super(BinaryLoss, self).__init__()
+#         self.bce = nn.BCEWithLogitsLoss()
+#         self.real = real
+#
+#     def forward(self, logits):
+#         if self.real:
+#             labels = torch.ones(logits.shape[0], 1)
+#         else:
+#             labels = torch.zeros(logits.shape[0], 1)
+#         if logits.is_cuda:
+#             labels = labels.cuda()
+#         return self.bce(logits, labels)
 
-    def forward(self, logits):
-        if self.real:
-            labels = torch.ones(logits.shape[0], 1)
-        else:
-            labels = torch.zeros(logits.shape[0], 1)
-        if logits.is_cuda:
-            labels = labels.cuda()
-        return self.bce(logits, labels)
+class BinaryLoss(nn.Module):
+
+    def __init__(self, resize=True):
+        super(BinaryLoss, self).__init__()
+        blocks = []
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[:4].eval())
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[4:9].eval())
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[9:16].eval())
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[16:23].eval())
+        for bl in blocks:
+            for p in bl:
+                p.requires_grad = False
+        self.blocks = torch.nn.ModuleList(blocks)
+        self.transform = torch.nn.functional.interpolate
+        self.mean = torch.nn.Parameter(torch.tensor([0.485, 0.456, 0.406]).view(1,3,1,1))
+        self.std = torch.nn.Parameter(torch.tensor([0.229, 0.224, 0.225]).view(1,3,1,1))
+        self.resize = resize
+
+    def forward(self, input, target):
+        if input.shape[1] != 3:
+            input = input.repeat(1, 3, 1, 1)
+            target = target.repeat(1, 3, 1, 1)
+        input = (input-self.mean) / self.std
+        target = (target-self.mean) / self.std
+        if self.resize:
+            input = self.transform(input, mode='bilinear', size=(224, 224), align_corners=False)
+            target = self.transform(target, mode='bilinear', size=(224, 224), align_corners=False)
+        loss = 0.0
+        x = input
+        y = target
+        for block in self.blocks:
+            x = block(x)
+            y = block(y)
+            loss += torch.nn.functional.l1_loss(x, y)
+        return loss
