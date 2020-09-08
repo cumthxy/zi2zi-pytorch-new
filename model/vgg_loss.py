5d7faeb36136e5bc8d9625d0bfba4dee1f73afbe
hahaha <cumthxy@163.com>
2020-09-08 21:07:29 +0800

    1070json

--- model/vgg_loss.py
+++ model/vgg_loss.py
@@ -6,6 +6,7 @@ l1_loss = nn.L1Loss()
 def get_model_and_losses(generate_img,target_img):
     layers_default = ['conv_2', 'conv_4', 'conv_6', 'conv_10', 'conv_14']
     vgg = models.vgg19(pretrained=True).features.eval()
+    vgg.features[0] = nn.Conv2d(1, 256, kernel_size=3, padding=1)
     model = nn.Sequential()
     vgg = copy.deepcopy(vgg)
     perceptual_loss=[]
